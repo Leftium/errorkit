4e130ab3e3f0fbbf3287be7861c035ecc452755e
Leftium <john@leftium.com>
2021-04-14 01:31:39 +0900

    Update $layout.svelte to load an error.

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,3 +1,12 @@
+<script context="module">
+	export async function load({ page, fetch, session, context }) {
+		return {
+			status: 499,
+			error: new Error(`Could not load`)
+		};
+	}
+</script>
+
 <script>
 	import '../app.css';
 </script>
